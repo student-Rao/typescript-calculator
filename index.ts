d96c56442ecb30d499a50163bcd74b2793a77309
Rao Wakeel <raowakeel895@gmail.com>
2024-07-02 07:46:44 +0500

    This is my first calculator with using typescript,nodejs etc

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+//shabang
+import inquirer from "inquirer";
+
+const calculator = await inquirer.prompt([
+    {message:"Enter your first number", type:"number",name:"firstnumber" },
+    {message:"Enter your second number", type:"number",name:"secondnumber" },
+    {message:"Please select your operator to this operation", 
+    type:"list", choices:["Addition","Subtraction","Multiplication","Division"],
+    name:"operator" 
+    }
+])
+
+if(calculator.operator==="Addition"){
+    console.log(calculator.firstnumber  + calculator.secondnumber );
+    
+}else if(calculator.operator==="Multiplication"){
+    console.log(calculator.firstnumber  * calculator.secondnumber );
+}else if(calculator.operator==="Division"){
+    console.log(calculator.firstnumber  / calculator.secondnumber );
+}else if(calculator.operator==="Subtraction"){
+    console.log(calculator.firstnumber  - calculator.secondnumber );
+}
+
+//console.log(chalk.blue.bgYellow.italic("Complete"),chalk.blue.bgYellow.italic("My Calculator"));
